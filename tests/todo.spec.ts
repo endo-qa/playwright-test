0626b7e41693ab6844910bb16b46125984b3a71b
endo-qa <267260658+endo-qa@users.noreply.github.com>
2026-03-13 23:16:30 +0900

    addTodo関数をhelpers/todo-actions.tsに切り出し refs #1

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,15 +1,11 @@
-import { test, expect, Page } from '@playwright/test';
-
-async function addTodo(page: Page, text: string) {
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill(text);
-  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-}
+import { test, expect } from '@playwright/test';
+import { addTodo } from './helpers/todo-actions';
 
 test('test', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/#/');
- await addTodo(page, '買い物リストを作成');
-await expect(page.getByTestId('todo-title').nth(0)).toBeVisible();
-await expect(page.getByTestId('todo-title').nth(0)).toContainText('買い物リストを作成');
+  await addTodo(page, '買い物リストを作成');
+  await expect(page.getByTestId('todo-title').nth(0)).toBeVisible();
+  await expect(page.getByTestId('todo-title').nth(0)).toContainText('買い物リストを作成');
   await addTodo(page, '牛乳を買う');
   await expect(page.getByTestId('todo-title').nth(1)).toBeVisible();
   await expect(page.locator('body')).toContainText('牛乳を買う');
